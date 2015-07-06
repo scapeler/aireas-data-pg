5bb3a496828741cf843bec987a030c9ceb2343a0
awiel <awiel@scapeler.com>
2015-07-06 23:02:45 +0200

    aireas-hist-json

---
+++ aireasdata-get-hist.sh
@@ -0,0 +1,22 @@
+
+##exit 0
+
+SYSTEMCODE=SCAPE604
+SYSTEMPATH=/opt/$SYSTEMCODE/
+
+LOGFILE=$SYSTEMPATH/$SYSTEMCODE/log/aireas-hist-data.log
+echo "Start procedure on: " `date` >>$LOGFILE
+
+mkdir -p $SYSTEMPATH/$SYSTEMCODE/aireas/aireas-hist/tmp
+mkdir -p $SYSTEMPATH/$SYSTEMCODE/log
+
+#cd  $SYSTEMPATH/$SYSTEMCODE/scape-aireas
+cd  $SYSTEMPATH/$SYSTEMCODE/aireas-data-pg
+#/usr/local/bin/node index.js aireas-hist-data.js >>$LOGFILE
+/usr/local/bin/node index.js aireas-hist2json.js /opt/SCAPE604/aireas/aireas-hist/tmp/aireas-hist25_cal.txt >>$LOGFILE
+
+#/usr/local/bin/node index.js aireas2sql.js >>$LOGFILE
+#/usr/local/bin/node index.js aireas2grid.js >>$LOGFILE
+#/usr/local/bin/node index.js aireas-signal.js >>$LOGFILE
+
+echo "End   procedure on: " `date` >>$LOGFILE
