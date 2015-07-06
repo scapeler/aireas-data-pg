cec2faa6e350b0b8ae027bf02af5b0f192afc28d
awiel <awiel@scapeler.com>
2015-07-06 20:33:47 +0200

    aireas-hist-data

--- aireas-hist-data.js
+++ aireas-hist-data.js
@@ -18,6 +18,8 @@ var airboxes = ['1_cal', '2_cal', '3_cal', '4_cal', '5_cal', '6_cal', '7_cal', '
 '11_cal', '12_cal', '13_cal', '14_cal', '15_cal', '16_cal', '17_cal', '18_cal', '19_cal', '20_cal', 
 '21_cal', '22_cal', '23_cal', '24_cal', '25_cal', '26_cal', '27_cal', '28_cal', '29_cal', '30_cal', 
 '31_cal', '32_cal', '33_cal', '34_cal', '35_cal', '36_cal', '37_cal', '38_cal', '39_cal', '40_cal' ];
+
+var fileImportIndex = 0;
 // **********************************************************************************
 
 
@@ -29,7 +31,7 @@ module.exports = {
 		aireasFileName 		= 'aireas-hist';
 
 		aireasLocalPathRoot = options.systemFolderParent + '/aireas/';
-		fileFolder 			= 'aireas';
+		fileFolder 			= 'aireas-hist';
 		tmpFolder 			= aireasLocalPathRoot + fileFolder + "/" + 'tmp/';
 
 		// create subfolders
@@ -38,14 +40,14 @@ module.exports = {
 		// 10-minuten reeksen met actuele AiREAS luchtmetingen. Verversing elke 10 minuten.
 	
 		for (var i=0;i<airboxes.length;i++) {
-			this.reqFile (aireasUrl+airboxes[i]+'.csv', aireasFileName+airboxes[i]+'.txt',	false, 'aireasdata');
+			this.reqFile (aireasUrl+airboxes[i]+'.csv', airboxes[i], aireasFileName+airboxes[i]+'.txt',	false, 'aireasdata');
 		}	
 
 		console.log('All retrieve actions are activated.');
 
 	},
 
-	reqFile: function (url, fileName, unzip, desc) {
+	reqFile: function (url, airbox, fileName, unzip, desc) {
 	
 	var _wfsResult=null;
 	console.log("Request start: " + desc + " (" + url + ")");
@@ -118,12 +120,12 @@ module.exports = {
   		}); 
 	}
 
-  	new StreamBuffer(request.get( { url: url }, function(error, response) {
+  	new StreamBuffer(request.get( { url: url, airbox:airbox }, function(error, response) {
 		console.log("Request completed: " + desc + " " );
 		var currDate = new Date();
 		var iso8601 = currDate.toISOString();
 
-		writeFile(tmpFolder, fileName, '{"retrievedDate": "' + iso8601 + '", "content":' + 
+		writeFile(tmpFolder, fileName, '{"airbox": "' + airbox + '", retrievedDate": "' + iso8601 + '", "content":' + 
 			_wfsResult + ' }');
 		})
   	);
